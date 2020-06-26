c33c6bfc94aae63d571ea66e2d5e9455071f0451
danielrosehill <daniel@danielrosehill.co.il>
2020-06-26 07:30:10 +0300

    Added script for backing up hosting files and MySQL to Linux local or NAS

---
+++ scripts/hostingbackup/Hosting_backup_to_Linux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+## Filesystem backup
+
+rsync -arvz -e 'ssh -p 12345' --delete --progress myuser@123.456.789.10:/home/mycpaneluser/public_html/ /home/me/backups/hosting/mysite/files/
+
+## MySQL backup
+
+rsync -arvz -e 'ssh -p 12345' --delete --progress myuser@123.456.789.10:/home/mycpaneluser/backup /home/me/backups/hosting/mysite/mysql/
+
+exit
+
