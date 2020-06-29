435c8c377ad80014f9cbb619623972e656bda43e
danielrosehill <daniel@danielrosehill.co.il>
2020-06-29 05:03:01 +0300

    Removed trailing slash in rsync command syntax in scrip

--- scripts/hostingbackup/Hosting_backup_to_Linux.sh
+++ scripts/hostingbackup/Hosting_backup_to_Linux.sh
@@ -2,7 +2,7 @@
 
 ## Filesystem backup
 
-rsync -arvz -e 'ssh -p 12345' --delete --progress myuser@123.456.789.10:/home/mycpaneluser/public_html/ /home/me/backups/hosting/mysite/files/
+rsync -arvz -e 'ssh -p 12345' --delete --progress myuser@123.456.789.10:/home/mycpaneluser/public_html /home/me/backups/hosting/mysite/files/
 
 ## MySQL backup
 
